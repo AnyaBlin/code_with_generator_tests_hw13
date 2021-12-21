0dbce95bea4ee68040c7be5566fcb2c3f3353f8a
AnyaBlin <93152022+AnyaBlin@users.noreply.github.com>
2021-12-21 19:29:45 +0300

    Update ProjectConfig.java

--- src/test/java/blin/config/ProjectConfig.java
+++ src/test/java/blin/config/ProjectConfig.java
@@ -2,21 +2,20 @@ package blin.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE)
+//@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-        "system:properties",
         "classpath:config/remote.properties"
 })
 public interface ProjectConfig extends Config {
 
-    @DefaultValue("chrome")
+   /* @DefaultValue("chrome")
     String browserName();
 
     @DefaultValue("96.0")
     String browserVersion();
 
     @DefaultValue("1920x1080")
-    String browserSize();
+    String browserSize(); */
 
     String login();
     String password();
